79bfb77bac086740cc10afb88ebb9ef7ca5db282
Jash <114028852+jashu24@users.noreply.github.com>
2024-12-18 14:59:42 -0800

    Update calculator.sh

--- calculator.sh
+++ calculator.sh
@@ -11,3 +11,18 @@ y=$((a - b))   # y = a - b
 # Print just x + y
 result=$((x + y))
 echo $result
+
+#python
+class Solution(object):
+    def calculate(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        return eval(s)  # This will evaluate the expression in the string
+
+# Example usage
+
+sol = Solution()
+result = sol.calculate("3 + 2")
+print(result) 
