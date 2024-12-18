bd2e4f5a25a9afd67711ae20097cb04cf095444a
Jash <114028852+jashu24@users.noreply.github.com>
2024-12-17 23:00:02 -0800

    update

--- calculator.sh
+++ calculator.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
-x=a+b
-y=a-b
-z=a/b
+# Assign values to variables
+a=1
+b=3
 
-a=1 b=3
+# Perform arithmetic operations
+x=$((a + b))   # x = a + b
+y=$((a - b))   # y = a - b
+
+# Print just x + y
+result=$((x + y))
+echo $result
