3234ccedc9275a20ec32a7afc8e7a23714f9723f
Jaswanth Setty <jashu@Jashs-Mac-Air.local>
2025-02-23 11:17:23 -0800

    first pythonfile

---
+++ MyLearnings/test.py
@@ -0,0 +1,9 @@
+#print("hello")
+#print("world")
+
+#os is a system software which is used to run an mulitple different softwares on the device like how much time it should run, data, storage, how many times and everything reloves around os; Also which IO does it take and how much memory should we allocate to it, everything reloves around it
+#
+
+a = 3
+b = 4
+print(a,b, sep='\n')
